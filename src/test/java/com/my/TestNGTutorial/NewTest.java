ee239b618a6493199a286200a0f68f05c5bc07b9
skarya <skarya_b03@hotmail.com>
2017-04-04 15:25:57 +0530

    updated

--- src/test/java/com/my/TestNGTutorial/NewTest.java
+++ src/test/java/com/my/TestNGTutorial/NewTest.java
@@ -10,25 +10,21 @@ public class NewTest {
 	    private WebDriver driver;		
 		@Test				
 		public void testEasy() {	
-			driver.get("http://google.com");  
-			String title = driver.getTitle();				 
-			AssertJUnit.assertTrue(title.contains("Google")); 		
+			 System.out.println("beforeTest run");	
 		}
 		
 		@Test				
 		public void Test1() {	
-			driver.get("http://google.com");  
-			String title = driver.getTitle();				 
-			AssertJUnit.assertTrue(title.contains("Google")); 		
+			 System.out.println("beforeTest run");		
 		}
 		@BeforeTest
 		public void beforeTest() {	
 			//System.setProperty("webdriver.gecko.driver","./geckodriver.exe");
 		    System.out.println("beforeTest run");
-			driver = new FirefoxDriver();  
+			
 		}		
 		@AfterTest
 		public void afterTest() {
-			driver.quit();			
+			System.out.println("afterTest run");		
 		}		
 }
